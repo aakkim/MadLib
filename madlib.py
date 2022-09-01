7cddbca957823f8d30505a9a0b4e99bc5ea7c8ca
Angela Kim <aangelakkim@gmail.com>
2022-08-31 20:12:38 -0400

    initial commit

---
+++ madlib.py
@@ -0,0 +1,22 @@
+
+
+place = input('Enter place: ')
+name = input('Enter name: ')
+adj1 = input('Enter adjective: ')
+noun1 = input('Enter noun: ')
+noun2 = input('Enter noun: ')
+adj2 = input('Enter adjective: ')
+adj3 = input('Enter adjective: ')
+noun3 = input('Enter noun: ')
+number = input('Enter number: ')
+noun4 = input('Enter noun: ')
+food = input('Enter food: ')
+verb = input('Enter verb: ')
+
+madlib = f"I went to the beach in {place} today with {name}. We had a {adj1} time. We first built \
+a {noun1} and then we ran around in the {noun2} for a little while. The water was a bit {adj2}. \
+The sun was very {adj3} so we made sure to wear a lot of {noun3}. We spent {number} hours there. \
+At the end of the day, we treated ourselves to a {noun4} and had {food} for dinner. I definitely want \
+to go to the beach again but next time I want to {verb}."
+
+print(madlib)
