9a8ec5810770864688f79dfe5bfc07f8b0edd0a4
Angela Kim <aangelakkim@gmail.com>
2022-09-03 20:37:53 -0400

    add madlib

---
+++ starSpangledBanner.py
@@ -0,0 +1,27 @@
+
+
+verb1 = input('Enter verb: ')
+noun1 = input('Enter noun: ')
+adverb = input('Enter adverb: ')
+verb2 = input('Enter verb (past tense): ')
+noun2 = input('Enter plural noun: ')
+noun3 = input('Enter another plural noun: ')
+adj1 = input('Enter adjective: ')
+noun4 = input('Enter plural noun: ')
+verb3 = input('Enter verb (ending in -ing): ')
+color = input('Enter color: ')
+noun5 = input('Enter plural noun: ')
+noun6 = input('Enter noun: ')
+noun7 = input('Enter another noun: ')
+interjection = input('Enter interjection: ')
+adj2 = input('Enter adjective: ')
+verb4 = input('Enter verb: ')
+adj3 = input('Enter adjective: ')
+adj4 = input('Enter another adjective: ')
+
+madlib = f"O say can you {verb1} by the dawn's early {noun1}. What so {adverb} we {verb2} at the twilight's last gleaming.\
+Whose broad {noun2} and bright {noun3} through the {adj1} fight, O'er the {noun4} we watched, were so gallantly {verb3}? And the \
+rockets' {color} glare, the {noun5} bursting in {noun6}, gave proof through the night that our {noun7} was still there; {interjection} \
+does that {adj2} banner yet {verb4}. O'er the land of the {adj3} and the home of the {adj4}." 
+
+print(madlib)
