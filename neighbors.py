ae12fafa45b94c11d13a34480ad8412129b6750d
Angela Kim <aangelakkim@gmail.com>
2022-09-03 20:55:17 -0400

    add madlib

---
+++ neighbors.py
@@ -0,0 +1,28 @@
+
+
+noun1 = input('Enter plural noun: ')
+noun2 = input('Enter noun: ')
+adj1 = input('Enter adjective: ')
+noun3 = input('Enter noun: ')
+noun4 = input('Enter another noun: ')
+noun5 = input('Enter another noun: ')
+adj2 = input('Enter adjective: ')
+noun6 = input('Enter noun: ')
+adj3 = input('Enter adjective: ')
+adverb = input('Enter adverb: ')
+noun7 = input('Enter noun: ')
+noun8 = input('Enter another noun: ')
+noun9 = input('Enter plural noun: ')
+noun10 = input('Enter noun: ')
+noun11 = input('Enter another noun: ')
+noun12 = input('Enter plural noun: ')
+adj4 = input('Enter adjective: ')
+noun13 = input('Enter plural noun: ')
+
+madlib = f"We have new {noun1} living in the {noun2} next door. He is a/an {adj1} salesman for a/an {noun3} company, and she teaches {noun4} in a private \
+{noun5}. Last night, we were invited to their {adj2} home for a potluck {noun6}. We brought a/an {adj3} casserole. After dinner, we went into their {adverb} \
+decorated family {noun7}, sat in front of their roaring {noun8}, and toasted {noun9}. Before we left, our host insisted on taking out his {noun10} -- which we \
+mistook for an ancient {noun11} -- to play a few country {noun12} on it. All in all, it was an {adj4} evening, and since then, the four of us have become \
+as thick as {noun13}."
+
+print(madlib)
